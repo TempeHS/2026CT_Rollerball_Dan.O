3afafacb5af31c087f2d632105e255f6e0e22cca
Dan O <dan.ozeri@education.nsw.gov.au>
2026-02-16 09:40:23 +1100

    WIP: Added Player Scripts ball protoype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2();
+    }
+}
